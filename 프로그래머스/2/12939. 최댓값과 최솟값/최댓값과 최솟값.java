c0a23db84ee116372f9e1506c3604b4ce5f28a00
Minsu Jeong <148730848+minsuje@users.noreply.github.com>
2024-10-06 23:12:57 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.57 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -9,7 +9,20 @@ class Solution {
             arr.add(Integer.parseInt(str[i]));
         
         answer = "" + Collections.min(arr) + " " + Collections.max(arr);
+        /*
         
+        String[] tmp = str.split(" ");
+        int min, max, n;
+        min = max = Integer.parseInt(tmp[0]);
+        for (int i = 1; i < tmp.length; i++) {
+                n = Integer.parseInt(tmp[i]);
+            if(min > n) min = n;
+            if(max < n) max = n;
+        }
+
+        return min + " " + max;
+
+        */
         return answer;
     }
 }
