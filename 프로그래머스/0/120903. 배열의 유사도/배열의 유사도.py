963eccfbff8a708f8cd89408eed6f068847a1238
Minsu Jeong <148730848+minsuje@users.noreply.github.com>
2025-04-16 14:50:08 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,6 +1,6 @@
 def solution(s1, s2):
-    count = 0
-    for x in s1:
-        if x in s2:
-            count +=1
-    return count
+   # count = 0
+    #for x in s1:
+    #    if x in s2:
+      #      count +=1
+    return len(set(s1)&set(s2))
