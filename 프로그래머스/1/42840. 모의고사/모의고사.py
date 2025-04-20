6b229fc7bd61520e1e045095577a9b182cbe0268
Minsu Jeong <148730848+minsuje@users.noreply.github.com>
2025-04-20 09:44:34 +0900

    [level 1] Title: 모의고사, Time: 3.58 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,19 @@
+def solution(answers):
+    p1 = [1,2,3,4,5]
+    p2 = [2,1,2,3,2,4,2,5]
+    p3 = [3,3,1,1,2,2,4,4,5,5]
+    scores = [0,0,0]
+    for i in range(len(answers)):
+        if p1[i % len(p1)] == answers[i]:
+            scores[0] += 1
+        if p2[i % len(p2)] == answers[i]:
+            scores[1] += 1
+        if p3[i % len(p3)] == answers[i]:
+            scores[2] += 1
+
+    answer = []
+    max_score = max(scores)
+    for idx in range(3):
+        if scores[idx] == max_score:
+            answer.append(idx+1)
+    return answer
