fa35540c8dd0679a7733d7216c7bd121bee31861
Minsu Jeong <148730848+minsuje@users.noreply.github.com>
2025-04-23 18:22:58 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.03 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,9 +1,9 @@
 def solution(s):
-    slist = s.split(" ")
-    answer = ""
-    for i in slist:
-        if i == slist[0]:
-            answer += i.capitalize()
-        else:
-            answer += (" "+ i.capitalize())
-    return answer
+    #slist = s.split(" ")
+   # answer = ""
+   # for i in slist:
+    #    if i == slist[0]:
+    #        answer += i.capitalize()
+    #    else:
+     #       answer += (" "+ i.capitalize())
+    return ' '.join(word.capitalize() for word in s.split(" "))
